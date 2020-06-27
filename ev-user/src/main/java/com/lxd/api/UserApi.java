7b311b416e3b249f0ac376ba0f3a28380ce05c5a
lxd7788 <964709569@qq.com>
2020-06-27 21:21:21 +0800

    222

--- ev-user/src/main/java/com/lxd/api/UserApi.java
+++ ev-user/src/main/java/com/lxd/api/UserApi.java
@@ -1,10 +1,6 @@
 package com.lxd.api;
 
 
-import com.lxd.pojo.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 /**
  * @Author: 98050
  * @Time: 2018-10-23 23:50
@@ -17,6 +13,6 @@ public interface UserApi {
      * @param password
      * @return
      */
-    @GetMapping("query")
-    User queryUser(@RequestParam("username")String username, @RequestParam("password")String password);
+//    @GetMapping("query")
+//    User queryUser(@RequestParam("username")String username, @RequestParam("password")String password);
 }
